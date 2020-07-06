0efd83eaf0c9fef211ca84a7ffc596a2f196905d
Diman <sxespade@gmail.com>
2020-07-06 15:17:42 +0300

    commit 5.1

--- app/src/main/java/com/example/myapplication/WeatherFragment.java
+++ app/src/main/java/com/example/myapplication/WeatherFragment.java
@@ -10,6 +10,8 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import static com.example.myapplication.MainActivity.PARCEL;
 
@@ -19,6 +21,7 @@ public class WeatherFragment extends Fragment {
     private String SITE = "https://ru.wikipedia.org/wiki/%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0";
     private String SITE2 = "https://ru.wikipedia.org/wiki/%D0%9B%D0%BE%D0%BD%D0%B4%D0%BE%D0%BD";
     private String SITE3 = "https://ru.wikipedia.org/wiki/%D0%9D%D1%8C%D1%8E-%D0%99%D0%BE%D1%80%D0%BA";
+    RecyclerView recyclerView;
 
     public static WeatherFragment create(Parcel parcel) {
         WeatherFragment f = new WeatherFragment();
@@ -40,10 +43,18 @@ public class WeatherFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View layout = inflater.inflate(R.layout.fragment_weather, container, false);
+
+
+
+        recyclerView = layout.findViewById(R.id.recycler_view);
         TextView cityNameView = layout.findViewById(R.id.city);
         Button button2 = layout.findViewById(R.id.button2);
         Parcel parcel = getParcel();
         cityNameView.setText(parcel.getCity());
+
+        String[] data = getResources().getStringArray(R.array.temps);
+        initRecyclerView(data);
+
 
         button2.setOnClickListener((v) -> {
             System.out.println("getIntent().getExtras().toString()");
@@ -75,6 +86,21 @@ public class WeatherFragment extends Fragment {
 
 
         return layout;
+    }
+
+    private void initRecyclerView(String[] data) {
+
+        // Эта установка служит для повышения производительности системы
+        recyclerView.setHasFixedSize(true);
+
+        // Будем работать со встроенным менеджером
+//        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+//        recyclerView.setLayoutManager(layoutManager);
+
+        // Установим адаптер
+        WeatherAdapter adapter = new WeatherAdapter(data);
+        recyclerView.setAdapter(adapter);
+
     }
 
 }
