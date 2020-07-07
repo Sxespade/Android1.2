a192e4797ca422f4c0e59864a23a3cd5d81f0c38
Diman <sxespade@gmail.com>
2020-07-07 19:02:06 +0300

    commit 6.1

--- app/src/main/java/com/example/myapplication/CityWeatherAdapter.java
+++ app/src/main/java/com/example/myapplication/CityWeatherAdapter.java
@@ -1,4 +1,51 @@
 package com.example.myapplication;
 
-public class CityWeatherAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CityWeatherAdapter extends RecyclerView.Adapter<CityWeatherAdapter.ViewHolder> {
+
+    private String[] dataSource;
+
+    public CityWeatherAdapter(String[] dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item2, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.getTextView().setText(dataSource[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataSource.length;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        private View v;
+        private TextView textView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            v = itemView;
+            textView = v.findViewById(R.id.city);
+        }
+
+        public TextView getTextView() {
+            return textView;
+        }
+
+    }
 }
