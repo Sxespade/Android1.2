6081951b40bd1009c00595d79c37fe33e7f2e928
Diman <sxespade@gmail.com>
2020-07-09 15:44:35 +0300

    commit 7.0

---
+++ app/src/main/java/com/example/myapplication/WeatherAdapter.java
@@ -0,0 +1,51 @@
+package com.example.myapplication;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.ViewHolder> {
+
+    private String[] dataSource;
+
+    public WeatherAdapter(String[] dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.getTextView().setText(dataSource[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataSource.length;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        private View v;
+        private TextView textView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            v = itemView;
+            textView = v.findViewById(R.id.temp);
+        }
+
+        public TextView getTextView() {
+            return textView;
+        }
+
+    }
+}
